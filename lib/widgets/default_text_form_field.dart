7b823d0117a085d06baa935909a879b794fa953f
NourElDienNwara <noureldiennwara312@gmail.com>
2026-02-04 00:59:15 +0200

    create favorite tap UI

---
+++ lib/widgets/default_text_form_field.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DefaultTextFormField extends StatelessWidget {
+  String hintText;
+  String? prefixIcon;
+  String? suffixIcon;
+
+  void Function(String)? onChanged;
+  TextEditingController? controller;
+  String? Function(String?)? validator;
+
+  DefaultTextFormField({
+    required this.hintText,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.onChanged,
+    this.controller,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        hintText: hintText,
+        prefixIcon: prefixIcon != null
+            ? SvgPicture.asset('assets/icons/$prefixIcon.svg')
+            : null,
+        suffixIcon: suffixIcon != null
+            ? SvgPicture.asset('assets/icons/$suffixIcon.svg')
+            : null,
+      ),
+
+      onChanged: onChanged,
+      controller: controller,
+      validator: validator,
+    );
+  }
+}
